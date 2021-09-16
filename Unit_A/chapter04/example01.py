73a74b90708f95adb4e3345d4c9916fc15633e6e
J. Evan Noynaert <noynaert@missouriwestern.edu>
2021-09-16 05:25:23 -0500

    Chapter 4 Turtle Graphics start

---
+++ Unit_A/chapter04/example01.py
@@ -0,0 +1,10 @@
+# very basic turtle graphics in Python 3
+
+import turtle
+
+screen = turtle.getscreen()
+tommy =  turtle.Turtle()
+
+turtle.forward(100)
+turtle.right(90)  #degrees
+turtle.forward(100)
